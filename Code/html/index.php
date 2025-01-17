123281b77107a7d19d062cd258b1453d5decf4fc
soutahasimoto <hasimoto.s511@gmail.com>
2025-01-17 14:13:50 +0900

    DB接続index.php

---
+++ Code/html/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+<?php
+//データベースのインスタンス名を指定
+$serverName = ".\\SQLEXPRESS";
+//接続情報を指定
+$connectionInfo = array("UID"=>"sa",
+                "PWD"=>"password",
+                "Database"=>"MyDB",
+                "CharacterSet"=>"UTF-8");
+//コネクションを確立
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+//クエリー文を指定
+$tsql = "SELECT * from staffs";
+//クエリーを実行
+$result = sqlsrv_query($conn, $tsql);
+?>
+  <table>
+<caption>スタッフリスト</caption>
+<?php
+    //実行結果を描画
+    while($row = sqlsrv_fetch_array($result)) {
+         printf("<tr><td class='hdr'>".$row['id']."</td>");
+        printf("<td>".$row['name']."</td></tr>");
+    }
+?>
+</table>
+<?php
+//クエリー結果の開放
+sqlsrv_free_stmt($result);
+//コネクションのクローズ
+sqlsrv_close($conn);
+?>
+    </body>
+</html>
